298ddc3723d9b249abb082b03b25f11d9e08e93a
Claudio Muniz dos Santos <claudiomunizsantos@gmail.com>
2025-10-30 19:47:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ let index = 0;
 
 function trocarImagem() {
   
-  imagens[index].classList.remove("active");
-  
+  imagens[index].classList.remove("active");  
   index = (index + 1) % imagens.length;
-
   imagens[index].classList.add("active");
 }
 
